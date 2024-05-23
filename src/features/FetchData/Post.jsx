270e65a60c02153d085cee287f52b0eb9c78dce9
eddremonts86 <eduardo.inerarte@gmail.com>
2024-05-23 09:24:06 +0200

    adding abortControler

--- src/features/FetchData/Post.jsx
+++ src/features/FetchData/Post.jsx
@@ -1,58 +1,62 @@
-
-import { useEffect, useState } from 'react';
-
+import { useEffect, useRef, useState } from "react";
 
 export default function Post() {
+  const [posts, setPosts] = useState(1);
+  const [postId, setPostId] = useState(null);
+  const abortController = useRef(null);
+  useEffect(() => {
+    const url = postId
+      ? `https://jsonplaceholder.typicode.com/posts/${postId}`
+      : "https://jsonplaceholder.typicode.com/posts";
 
-    const [posts, setPosts] = useState(1);
-    const [postId, setPostId] = useState(null);
+    const fetchPost = async () => {
+      abortController.current?.abort();
+      abortController.current = new AbortController();
 
-    useEffect(() => {
+      try {
+        const response = await fetch(url, {
+          signal: abortController.current.signal,
+        });
+        const data = await response.json();
+        setPosts(postId ? [data] : data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPost();
 
-        const url = postId
-            ? `https://jsonplaceholder.typicode.com/posts/${postId}`
-            : 'https://jsonplaceholder.typicode.com/posts';
+    return () => {
+      setPosts(null);
+    };
+  }, [postId]);
 
-        const fetchPost = async () => {
-            try {
-                const respone = await fetch(url)
-                const data = await respone.json();
-                setPosts(data);
-                alert('Post fetched successfully');
+  const handleClick = () => {
+    setPostId((prev) => prev + 1);
+  };
 
-            } catch (error) {
-                alert('Failed to fetch post');
-
-            }
-
-        }
-        fetchPost();
-
-        return () => {
-            setPosts(null);
-        }
-
-    }, [postId]);
-
-
-    const handleClick = () => {
-        setPostId(1);
-    }
-
-
-    if (!posts) return <div>Loading...</div>;
-
-    return (
-        <>
-            <div>
-                <button className="border-1 bg-red-800 px-10 py-4 rounded-xl" onClick={handleClick}>Fetch Post</button>
-            </div>
-            {posts.map((post) => (
-                <div className='p-3 rounded-md bg-blue-3' key={Post.id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.body}</p>
-                </div>
-            ))}
-        </>
-    )
+  return (
+    <>
+      <div>
+        <button
+          className="border-1 bg-red-800 px-10 py-4 rounded-xl"
+          onClick={handleClick}
+        >
+          Fetch Post
+        </button>
+      </div>
+      {!posts || !posts.length ? (
+        <div>Loading...</div>
+      ) : (
+        posts.map((post) => (
+          <div
+            className="p-3 rounded-md bg-blue-3"
+            key={Post.id}
+          >
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))
+      )}
+    </>
+  );
 }
